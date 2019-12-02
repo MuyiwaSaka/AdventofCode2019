2503368df3a19a5190451b0c999c16ad6b5b2014
Muyiwa Saka <sakamuyiwa@gmail.com>
2019-12-02 20:47:15 +0100

    Refactored Day One file.

--- DayOneTaskOne.py
+++ DayOneTaskOne.py
@@ -5,8 +5,7 @@ def round_down(numb, decimals=0):
     return math.floor(numb*multiplier)/multiplier
 
 def get_fuel_module(modcnt):
-    m = modcnt/3
-    return round_down(m)-2
+    return round_down(modcnt/3)-2
 
 def find_last_fuel_value(fval):
     fr=fval
@@ -17,11 +16,12 @@ def find_last_fuel_value(fval):
             ft = ft + fr
     return ft
 
+# Calculate the values and print the output
 santa_fuel=0
 santa_neg_fuel = 0
 f = open("day1 input.txt")
 for r in f:
     santa_fuel = santa_fuel + get_fuel_module(int(r))
     santa_neg_fuel = santa_neg_fuel + find_last_fuel_value(int(r))
-print(santa_fuel)
-print(santa_neg_fuel)
+print("Santa's fuel is {}".format(int(santa_fuel)))
+print("Santa's Recursive Fuel is {}".format(int(santa_neg_fuel)))
