c10ac5bcd57639c54884ef71b09d736117626537
Muyiwa Saka <sakamuyiwa@gmail.com>
2019-12-02 22:04:31 +0100

    Day 2 First Gold Star

--- DayTwo.py
+++ DayTwo.py
@@ -1,6 +1,6 @@
 import math
 
-def run_magic_computer(fg):
+def interprete_intcode(fg):
     print("Input: {}".format(fg))
     machine = fg.split(",")
     pstate = 0
@@ -19,10 +19,10 @@ def run_magic_computer(fg):
     print machine
 
 #Test The Machine using prior test data
-run_magic_computer("1,0,0,0,99") # validated
-run_magic_computer("2,3,0,3,99") # validated
-run_magic_computer("2,4,4,5,99,0") # validated
-run_magic_computer("1,1,1,4,99,5,6,0,99") # validated
+interprete_intcode("1,0,0,0,99") # validated
+interprete_intcode("2,3,0,3,99") # validated
+interprete_intcode("2,4,4,5,99,0") # validated
+interprete_intcode("1,1,1,4,99,5,6,0,99") # validated
 
 print("RUNNING MAIN CODE")
 h= open("day2input.txt","r")
@@ -32,4 +32,4 @@ brkdwn[1]="12"
 brkdwn[2]="2"
 feed = ",".join([str(x) for x in brkdwn])
 
-run_magic_computer(feed)
+interprete_intcode(feed)
