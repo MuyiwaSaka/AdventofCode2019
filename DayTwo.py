42b9b9243eed3d333a36affab399c95448f8ba8a
Muyiwa Saka <sakamuyiwa@gmail.com>
2019-12-02 22:45:01 +0100

    Day 2 Second Gold Star code

--- DayTwo.py
+++ DayTwo.py
@@ -2,33 +2,52 @@ import math
 
 def interprete_intcode(fg):
     machine = fg.split(",")
-    pstate = 0
+    ptr = 0
     running = 1
     while running > 0:
-        chk = int(machine[pstate])
+        chk = int(machine[ptr])
         if chk == 1:
-            machine[int(machine[pstate+3])] = int(machine[int(machine[pstate+1])]) + int(machine[int(machine[pstate+2])])
+            machine[int(machine[ptr + 3])] = str(int(machine[int(machine[ptr + 1])]) + int(machine[int(machine[ptr + 2])]))
         elif chk == 2:
-            machine[int(machine[pstate+3])] = int(machine[int(machine[pstate+1])]) * int(machine[int(machine[pstate+2])])
-        elif chk == 99 :
+            machine[int(machine[ptr + 3])] = str(int(machine[int(machine[ptr + 1])]) * int(machine[int(machine[ptr + 2])]))
+        elif chk == 99:
             running = 0
         else:
             running = 0
-        pstate = pstate + 4
-    print machine
+        ptr = ptr + 4
+    return machine
 
-#Test The Machine using prior test data
+
+# Test The Machine using prior test data
 interprete_intcode("1,0,0,0,99") # validated
 interprete_intcode("2,3,0,3,99") # validated
 interprete_intcode("2,4,4,5,99,0") # validated
 interprete_intcode("1,1,1,4,99,5,6,0,99") # validated
 
-print("RUNNING MAIN CODE")
-h= open("day2input.txt","r")
-f= h.read()
-brkdwn = f.split(",")
-brkdwn[1]="12"
-brkdwn[2]="2"
-feed = ",".join([str(x) for x in brkdwn])
+def generate_output(noun,verb):
+    h = open("day2input.txt","r")
+    f = h.read()
+    lst = f.split(",")
+    lst[1], lst[2] = str(noun), str(verb)
+    feed = ",".join([str(x) for x in lst])
+    output = interprete_intcode(feed)
+    return output
 
-interprete_intcode(feed)
+print("First Star Code")
+print(generate_output(12, 2))
+
+print("Second Star Code")
+results = {"Noun":0, "Verb":0}
+for x in range(99):
+    for y in range (99):
+        output = generate_output(x,y)
+        v=  int(output[0])
+        if v == 19690720:
+            results["Noun"],results["Verb"] = x, y
+            break
+        else:
+            continue
+        break
+
+finalvar = 100 * results["Noun"] + results["Verb"]
+print(finalvar)
